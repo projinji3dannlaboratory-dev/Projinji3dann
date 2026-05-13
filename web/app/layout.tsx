f33abbb8e8e34cc6d8f686a7185eabf5f9ae1694
projinji3dannlaboratory-dev <projinji3dann.laboratory@gmail.com>
2026-05-14 01:11:08 +0900

    feat: Mount ShareWidget in root layout

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -10,6 +10,7 @@ import {
   siteJsonLd,
 } from "@/components/seo/json-ld";
 import { SITE_CONFIG, isAdsenseEnabled } from "@/lib/site-config";
+import ShareWidget from "@/components/layout/share-widget";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -77,6 +78,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </ThemeProvider>
         <AnalyticsScripts />
         <JsonLd data={[siteJsonLd(), organizationJsonLd()]} />
+        <ShareWidget />
       </body>
     </html>
   );
